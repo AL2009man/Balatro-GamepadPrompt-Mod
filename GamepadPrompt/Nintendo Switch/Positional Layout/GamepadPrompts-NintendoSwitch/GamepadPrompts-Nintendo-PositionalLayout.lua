e64bb959d7d0a3c88c9a6345ff9f52d973b18ece
Al. Lopez <67606569+AL2009man@users.noreply.github.com>
2024-12-16 22:49:21 -0500

    another emergency restoration

---
+++ GamepadPrompt/Nintendo Switch/Positional Layout/GamepadPrompts-NintendoSwitch/GamepadPrompts-Nintendo-PositionalLayout.lua
@@ -0,0 +1,19 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Gamepad Prompts - Nintendo Switch (Positional Layout)
+--- MOD_ID: GamepadPrompts-Nintendo-PositionalLayout
+--- MOD_AUTHOR: [AL2009man]
+--- MOD_DESCRIPTION: This is a simple mod that replaces Xbox One button prompt textures infavor of Nintendo Switch prompts. This version will be based on the Positional Face button Layout.
+--- BADGE_COLOUR: D92F20
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0812d]
+--- VERSION: 1.0.1
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+sendDebugMessage("Launching Gamepad Button Prompt Icon!")
+
+SMODS.Atlas{key = "gamepad_ui", path = "gamepad_ui.png", px = 32, py = 32, prefix_config = { key = false } }
+
+----------------------------------------------
+------------MOD CODE END----------------------
